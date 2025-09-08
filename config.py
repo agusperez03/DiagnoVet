b6de33ecd77ec0a8b691440f4311fc755418a67f
agusperez03 <agustinpereh03@gmail.com>
2025-09-07 23:42:20 -0300

    deploy

--- config.py
+++ config.py
@@ -2,6 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'una-clave-secreta-muy-dificil'
-    # Conexión a la base de datos PostgreSQL que crearemos con Docker
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user:mysecretpassword@localhost:5432/diagnovet'
+
+    # Lee la URL de la base de datos desde la variable de entorno
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL or 'postgresql://user:mysecretpassword@localhost:5432/diagnovet'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
